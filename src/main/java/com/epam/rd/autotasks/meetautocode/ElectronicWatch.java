be30264e1e3c9b197fb3579c8555cf5d159c02c4
Beata Podgorska <beata.podgorska26@gmail.com>
2022-04-07 19:14:59 +0200

    Solved

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,16 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        int ss = seconds % 60;
+        int allMinutes = seconds / 60;
+        int mm = allMinutes % 60;
+        int h = allMinutes / 60;
 
+        while(h>23){
+            if(h==24){ h = 0; break;}
+            h-=24;
+        }
+
+        System.out.format("%d:%02d:%02d",h,mm,ss);
     }
 }
